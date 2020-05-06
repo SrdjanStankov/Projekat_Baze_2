f7056d46838ba9e96eaa0a1a40e13440a743ebb2
Srdjan Stankov <stankovsr@gmail.com>
2020-05-06 17:52:58 +0200

    Added Null validation rule

---
+++ Projekat/WpfUI/Model/ValidationRules/NullValidationRule.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WpfUI.Model.ValidationRules
+{
+    public class NullValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var exp = value as BindingExpression;
+            if (exp.DataItem is null)
+            {
+                return new ValidationResult(false, "Polje je obavezno.");
+            }
+            var val = exp.DataItem.GetType().GetProperty(exp.ParentBinding.Path.Path).GetValue(exp.DataItem, null);
+            return val is null
+                ? new ValidationResult(false, "Polje je obavezno.")
+                : value is null ? new ValidationResult(false, "Polje je obavezno.") : ValidationResult.ValidResult;
+        }
+    }
+}

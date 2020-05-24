de7f8c2b699c763abcbbb13156e741c57d274b13
Srdjan Stankov <stankovsr@gmail.com>
2020-05-24 16:18:11 +0200

    Updated procedure creation

--- Sql/Create Procedure.sql
+++ Sql/Create Procedure.sql
@@ -1,5 +1,7 @@
+use Projekat_Baze_2;
+go
 create procedure SelectPosadaAndMornar as
 begin
-	select * from Posada left outer join Mornar on Posada.ID = Mornar.ID order by Posada.ID;
+	select Posada.ID, Posada.Ime, Posada.Kapacitet, Posada.JMBG_Kormilar, Posada.JMBG_Kapetan, Posada.IDBroda  from Posada left outer join Mornar on Posada.ID = Mornar.ID order by Posada.ID;
 	return;
 end
